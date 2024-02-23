a99b59833c987a7cdf595e4386d651b87bda9a43
hariompandey <hariompandey9360@gmail.com>
2024-02-23 16:33:37 +0530

    adding components and utils in which material icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from '@mui/material';
+import {Feed,Navbar,ChannelDetail,VideoDetails,SearchFeed} from './Components'
 
-const main = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
-}
 
-export default main
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetails />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/Components/ChannelDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const channeldetail = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default channeldetail
+
