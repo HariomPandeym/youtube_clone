be6803cd4ad032faf9748518244ad581bb728336
hariompandey <hariompandey9360@gmail.com>
2024-02-25 13:11:39 +0530

    adding filees

--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromAPI } from "../Utils/FetchFromApi";
 
 const SearchFeed = () => {
-  return <div>searchfeed</div>;
+
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        
+      Search Results for:
+        <span style={{ color: "#F31503" }}>Videos</span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
