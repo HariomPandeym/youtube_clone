b2aa7f66c2344dd717027a29a2e0a34454c76f95
hariompandey <hariompandey9360@gmail.com>
2024-02-25 13:36:07 +0530

    modification files

--- src/Components/ChannelDetail.jsx
+++ src/Components/ChannelDetail.jsx
@@ -1,3 +1,5 @@
+
+
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
@@ -10,8 +12,6 @@ const ChannelDetail = () => {
   const [videos, setVideos] = useState(null);
 
   const { id } = useParams();
-  console.log(channelDetail,videos)
-  
 
   useEffect(() => {
     const fetchResults = async () => {
@@ -19,9 +19,7 @@ const ChannelDetail = () => {
 
       setChannelDetail(data?.items[0]);
 
-      const videosData = await fetchFromAPI(
-        `search?channelId=${id}&part=snippet%2Cid&order=date`
-      );
+      const videosData = await fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`);
 
       setVideos(videosData?.items);
     };
@@ -32,18 +30,15 @@ const ChannelDetail = () => {
   return (
     <Box minHeight="95vh">
       <Box>
-        <div
-          style={{
-            height: "300px",
-            background:
-              "linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)",
-            zIndex: 10,
-          }}
-        />
-        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+        <div style={{
+          height:'300px',
+          background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          zIndex: 10,
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-93px" />
       </Box>
       <Box p={2} display="flex">
-        <Box sx={{ mr: { sm: "100px" } }} />
+      <Box sx={{ mr: { sm: '100px' } }}/>
         <Videos videos={videos} />
       </Box>
     </Box>
--- src/Components/SearchFeed.jsx
+++ src/Components/SearchFeed.jsx
@@ -1,25 +1,55 @@
+// import { useState, useEffect } from "react";
+// import { Box, Typography } from "@mui/material";
+// import { Videos } from "./";
+// import { fetchFromAPI } from "../Utils/FetchFromApi";
+
+// const SearchFeed = () => {
+
+//   const [videos, setVideos] = useState([]);
+//   useEffect(() => {
+//     fetchFromAPI(`search?part=snippet&q=`).then((data) =>
+//       setVideos(data.items)
+//     );
+//   }, []);
+//   return (
+//     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+//       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        
+//       Search Results for:
+//         <span style={{ color: "#F31503" }}>Videos</span>
+//       </Typography>
+
+//       <Videos videos={videos} />
+//     </Box>
+//   );
+// };
+
+// export default SearchFeed;
 import { useState, useEffect } from "react";
-import { Box, Typography } from "@mui/material";
-import { Videos } from "./";
+import { Typography, Box } from "@mui/material";
+import { useParams } from "react-router-dom";
+
 import { fetchFromAPI } from "../Utils/FetchFromApi";
+import { Videos } from "./";
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams();
 
-  const [videos, setVideos] = useState([]);
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=`).then((data) =>
-      setVideos(data.items)
-    );
-  }, []);
-  return (
-    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        
-      Search Results for:
-        <span style={{ color: "#F31503" }}>Videos</span>
-      </Typography>
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+  }, [searchTerm]);
 
-      <Videos videos={videos} />
+  return (
+    <Box p={2} minHeight="95vh">
+      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
+        Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={videos} />}
+      </Box>
     </Box>
   );
 };
